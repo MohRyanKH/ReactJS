40eb1e61bbad4f7af30dd2058e1481ba37c6cb2e
MohRyanKH <rykhalda@gmail.com>
2023-01-16 19:55:20 +0700

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <i class="bi bi-3-circle-fill"></i>
+
+    </div>
+  );
+}
+
+export default App;
